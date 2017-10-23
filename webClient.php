10e48923619d1f2ad981f92ea55ead2fda5eee36
赵亮 <zhaoliang@zhaoliangdeMacBook-Pro-2.local>
2017-10-23 16:09:17 +0800

    web client

--- webClient.php
+++ webClient.php
@@ -9,28 +9,17 @@ echo "send...", PHP_EOL;
 
 
 
-function getClient()
+
+$client = new swoole_client(SWOOLE_SOCK_TCP);
+if (!$client->connect('127.0.0.1', 9501, -1))
 {
-    $client = new swoole_client(SWOOLE_SOCK_TCP);
-    if (!$client->connect('127.0.0.1', 9501, -1))
-    {
-        exit("connect failed. Error: {$client->errCode}\n");
-    }
-    $res = $client->getSocket();
-    return $client;
+    exit("connect failed. Error: {$client->errCode}\n");
 }
-$client = getClient();
-$count = 0;
-//$client->set(array('open_eof_check' => true, 'package_eof' => "\r\n\r\n"));
-//$client = new swoole_client(SWOOLE_SOCK_UNIX_DGRAM, SWOOLE_SOCK_SYNC); //同步阻塞
-//if (!$client->connect(dirname(__DIR__).'/server/svr.sock', 0, -1, 1))
-var_dump($client->getsockname());
+
+
 $client->send($uuid.",".$userId);
-//for($i=0; $i < 3; $i ++)
-{
-    //echo $client->recv();
-    sleep(2);
-}
+sleep(2);
+
 $client->close();
 
 ?>
