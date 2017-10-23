a6b2bf7d98112b50a36d95775804a0ffd31b2bdb
赵亮 <zhaoliang@zhaoliangdeMacBook-Pro-2.local>
2017-10-23 15:58:50 +0800

    web client

--- webClient.php
+++ webClient.php
@@ -1,5 +1,7 @@
 <?php
 //发起client请求
+$uuid = 2345;
+$userId = 222;
 $client = new swoole_client(SWOOLE_SOCK_UDP, SWOOLE_SOCK_SYNC);
 $client->connect('127.0.0.1', 9501);
 $client->send($uuid.",".$userId);
