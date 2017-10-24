8d73a101458065fa1e6f7e3b64fdadfce7e36586
赵亮 <zhaoliang@zhaoliangdeMacBook-Pro-2.local>
2017-10-24 11:07:35 +0800

    test php rabbitmq

---
+++ receive.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: zhaoliang
+ * Date: 17/10/24
+ * Time: 上午10:55
+ */
+require_once __DIR__ . '/vendor/autoload.php';
+
+use PhpAmqpLib\Connection\AMQPStreamConnection;
+
+$connection = new AMQPStreamConnection('localhost', 5672, 'guest', 'guest');
+$channel = $connection->channel();
+
+$channel->queue_declare('hello', false, false, false, false);
+
+echo ' [*] Waiting for messages. To exit press CTRL+C', "\n";
+
+$callback = function($msg) {
+    echo " [x] Received ", $msg->body, "\n";
+};
+
+$channel->basic_consume('hello', '', false, true, false, false, $callback);
+
+while(count($channel->callbacks)) {
+    $channel->wait();
+}
---
+++ send.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: zhaoliang
+ * Date: 17/10/24
+ * Time: 上午10:53
+ */
+require_once __DIR__ . '/vendor/autoload.php';
+
+use PhpAmqpLib\Connection\AMQPStreamConnection;
+use PhpAmqpLib\Message\AMQPMessage;
+
+$connection = new AMQPStreamConnection('localhost', 5672, 'guest', 'guest');
+$channel = $connection->channel();
+
+$channel->queue_declare('hello', false, false, false, false);
+
+$msg = new AMQPMessage('Hello World!');
+$channel->basic_publish($msg, '', 'hello');
+
+echo " [x] Sent 'Hello World!'\n";
+
+$channel->close();
+$connection->close();
