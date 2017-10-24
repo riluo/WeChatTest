b7e57e14e9af74465991370be811e1b981e49028
赵亮 <zhaoliang@zhaoliangdeMacBook-Pro-2.local>
2017-10-24 14:49:40 +0800

    add chat and self consumer

---
+++ receive_self.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: zhaoliang
+ * Date: 17/10/24
+ * Time: 下午1:38
+ */
+require_once __DIR__ . '/vendor/autoload.php';
+
+use PhpAmqpLib\Connection\AMQPStreamConnection;
+
+$connection = new AMQPStreamConnection('localhost', 5672, 'guest', 'guest');
+$channel = $connection->channel();
+
+$channel->queue_declare('self', false, false, false, false);
+
+echo ' [*] Waiting for contact. To exit press CTRL+C', "\n";
+
+
+$callback = function($msg) {
+    echo " [x] Received contact", $msg->body, "\n";
+};
+
+$channel->basic_consume('self', '', false, true, false, false, $callback);
+
+while(count($channel->callbacks)) {
+    $channel->wait();
+}
