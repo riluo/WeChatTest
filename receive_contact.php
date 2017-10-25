c1a1a64d846d1c927af4e937155612eeb4acd37e
赵亮 <zhaoliang@zhaoliangdeMacBook-Pro-2.local>
2017-10-25 10:41:57 +0800

    receive data from queue

--- receive_contact.php
+++ receive_contact.php
@@ -16,9 +16,27 @@ $channel->queue_declare('contact', false, false, false, false);
 
 echo ' [*] Waiting for contact. To exit press CTRL+C', "\n";
 
-
-$callback = function($msg) {
+$pdo = new PDO("mysql:host=localhost;dbname=sd_chat","root","Sunland16");
+$callback = function($msg) use($pdo) {
     echo " [x] Received contact", $msg->body, "\n";
+
+    $arr = explode("$%",$msg->body);
+    $userId = $arr[0];
+    $uin = $arr[1];
+    $username = $arr[2];
+    $nickName = $arr[3];
+    $remarkName = $arr[4];
+    $sex = $arr[5];
+    $avatar = $arr[6];
+
+    $stmt=$pdo->prepare("SELECT * from friends where nickName = '".$nickName."' and remarkName = '".$remarkName."' and who = '".$uin."'");
+    $stmt->execute();
+
+    if($stmt->rowCount()>0) {
+        $pdo->exec("UPDATE friends set userName='".$username."',updateTime='".date("Y-m-d H:i:s",time())."' where nickName = '".$nickName."' and remarkName = '".$remarkName."' and who = '".$uin."'");
+    } else {
+        $pdo->exec("insert into friends(userName,nickName,remarkName,sex,headImgUrl,who,whoId, createTime,updateTime) values('".$username."','".$nickName."','".$remarkName."','".$sex."','".$avatar."','".$uin."','".$userId."','".date("Y-m-d H:i:s",time())."','".date("Y-m-d H:i:s",time())."')");
+    }
 };
 
 $channel->basic_consume('contact', '', false, true, false, false, $callback);
--- receive_self.php
+++ receive_self.php
@@ -16,9 +16,30 @@ $channel->queue_declare('self', false, false, false, false);
 
 echo ' [*] Waiting for self. To exit press CTRL+C', "\n";
 
-
-$callback = function($msg) {
+$pdo = new PDO("mysql:host=localhost;dbname=sd_chat","root","Sunland16");
+$callback = function($msg) use($pdo) {
     echo " [x] Received self", $msg->body, "\n";
+
+    $arr = explode("$%",$msg->body);
+    $userId = $arr[0];
+    $uin = $arr[1];
+    $sid = $arr[2];
+    $skey = $arr[3];
+    $passTicket = $arr[4];
+    $deviceId = $arr[5];
+    $username = $arr[6];
+    $nickname = $arr[7];
+    $avatar = $arr[8];
+
+
+    $stmt=$pdo->prepare("SELECT * from self where uin = '".$uin."'");
+    $stmt->execute();
+
+    if($stmt->rowCount()>0) {
+        $pdo->exec("UPDATE self set sid='".$sid."',skey='".$skey."',passTicket='".$passTicket."',deviceId='".$deviceId."',username='".$username."',nickname='".$nickname."',updateTime='".date("Y-m-d H:i:s",time())."' where uin = '".$uin."'");
+    } else {
+        $pdo->exec("insert into self(userId,uin,sid,skey,deviceId,passTicket,username,headImgUrl,nickname, createTime,updateTime) values('".$userId."','".$uin."','".$sid."','".$skey."','".$deviceId."','".$passTicket."','".$username."','".$avatar."','".$nickname."','".date("Y-m-d H:i:s",time())."','".date("Y-m-d H:i:s",time())."')");
+    }
 };
 
 $channel->basic_consume('self', '', false, true, false, false, $callback);
